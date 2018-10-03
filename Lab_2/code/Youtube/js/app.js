9bb7497026f5ae8a740f4c34fbdbcd1a72d8decd
Murali <murali.24.4.91@gmail.com>
2018-10-03 18:18:33 -0500

    Lab 2 added documentation

--- Lab_2/code/Youtube/js/app.js
+++ Lab_2/code/Youtube/js/app.js
@@ -1,8 +1,13 @@
 var app = angular.module('youTubeApp', []);
 app.controller('youTubeCtrl', function ($scope,$http) {
+    //Declaring the variables
     $scope.searchTerm = "";
     $scope.results = [];
     $scope.isVideo = false;
+    /*
+    * This method will call when the user clicks on search button after entering the search key word.
+    * In this method we will call the Youtube API and get the results and assign the result to $scope.results variable.
+    */
     $scope.search = function () {
         console.log($scope.searchTerm);
         $scope.isVideo = false;
@@ -22,7 +27,11 @@ app.controller('youTubeCtrl', function ($scope,$http) {
             console.log(response);
         });
     }
-    
+
+    /*
+    * This method will call when the user clicks on thumbnail of a video.
+    * In this method we are modifying the source of iframe which is hidden and make it visible by changing the $scope.isVideo to true.
+    */
     $scope.play = function (videoId) {
         $scope.isVideo = true;
         var d=document.getElementById("youtubePlay");
